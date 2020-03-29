6f2585d10c0c20ccd7f25219438d8e0a708973c8
Drnd1 <drndivoje@users.noreply.github.com>
2020-03-29 15:49:13 +0200

    Initial commit

---
+++ src/bcp.rs
@@ -0,0 +1,110 @@
+extern crate chrono;
+
+use std::fs;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::Instant;
+
+pub fn backup(path: &str) {
+    if Path::new(path).exists() {
+        let backup_folder = format!("/home/drndivoje/backup-{}", chrono::offset::Local::now().to_string());
+        let _bcp_folder = fs::create_dir(backup_folder.as_str());
+        match _bcp_folder {
+            Ok(_bcp_folder) => {
+                println!("Starting backup");
+                match copy(Path::new(path), Path::new(backup_folder.as_str())) {
+                    Ok(_result) => {
+                        _result.save(backup_folder.as_str()).expect("failed to save bcp.info file");
+                    },
+                    Err(_e) => {
+                        println!("Backup failed. Failed to create backup folder.")
+                    }
+                }
+
+            }
+            Err(_e) => {
+                println!("Backup failed. Failed to create backup folder.")
+            }
+        }
+    } else {
+        println!("Backup failed. Folder {} does not exist.", path)
+    }
+}
+
+#[derive(Debug)]
+struct BackupStatistic {
+    duration_ms: u128,
+    files_count: i32,
+    start_time: Instant,
+}
+
+impl BackupStatistic {
+    fn save(&self, path:&str) -> std::io::Result<()> {
+        let mut file = fs::File::create(format!("{}/bcp.info", path))?;
+        file.write_all( format!("{:?}", self).as_bytes())?;
+        Ok(())
+    }
+    fn file_count(&mut self) {
+        self.files_count += 1;
+    }
+
+    fn new() -> BackupStatistic {
+        BackupStatistic{duration_ms: 0, files_count:0, start_time: Instant::now()}
+    }
+
+    fn stop(&mut self) {
+        self.duration_ms = self.start_time.elapsed().as_millis();
+    }
+
+}
+
+
+fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<BackupStatistic, std::io::Error> {
+    let mut bcp_stats = BackupStatistic::new();
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                        bcp_stats.file_count();
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    bcp_stats.stop();
+    Ok(bcp_stats)
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use clap::{App, Arg, SubCommand};
+use human_panic::setup_panic;
+
+mod bcp;
+
+
+fn main() {
+    setup_panic!();
+    let matches = App::new("BCP is short for BaCkuP")
+        .version("0.0.1")
+        .author("Ivan Doskovic")
+        .about("Simple backup tool")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true))
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+        .subcommand(SubCommand::with_name("run")
+            .about("executing backup of given folder")
+            .version("1.3")
+            .arg(Arg::with_name("input")
+                .short("i")
+                .help("the path to the folder to be backed up")
+                .required(true)
+                .takes_value(true)))
+        .get_matches();
+
+    // TODO: setup configuration
+    // let config = matches.value_of("config").unwrap_or("default.conf");
+
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let input_folder = matches.value_of("input").unwrap();
+        bcp::backup(input_folder)
+    }
+}
